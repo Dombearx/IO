79e3c21bcb0c5fe9324daef5aa844052e3b9d2fc
unknown <inf132207@lab-44-14.cs.put.poznan.pl>
2018-10-31 10:35:01 +0100

    nowe wyswietlane teksty

--- program.java
+++ program.java
@@ -8,6 +8,7 @@ public class HelloWorld
 	}
 	
 	public static void main(String[] args) {
+		int zmienna = 0;
 		System.out.println("Hello World!");
 		System.out.println("Hello World2!");
 		System.out.println("Hello World3!");
