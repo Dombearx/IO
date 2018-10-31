1a9324b4a47099f462218cea82d54fce0f2b2229
unknown <inf132207@lab-44-14.cs.put.poznan.pl>
2018-10-31 10:31:43 +0100

    nowa metoda - dodawanie

--- program.java
+++ program.java
@@ -3,6 +3,10 @@
 
 public class HelloWorld
 {
+	public int dodawanie(int a, int b){
+		return a + b;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World2!");
